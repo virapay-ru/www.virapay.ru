7d70f8404b43ceb3c0fd2773e1c497675ca1b575
virapay-ru <virapay@yandex.ru>
2020-07-08 19:29:59 +0300

    work in progress

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
-﻿let backend = new JSONRPC2.RemoteProxyObject(
+﻿// backend
+
+let backend = new JSONRPC2.RemoteProxyObject(
 	// using JSON RPC over HTTP
 	new JSONRPC2.Transports.HTTP(
 		'https://virapay.herokuapp.com/rpc/json',
@@ -7,6 +9,8 @@
 		 }
 	)
 )
+
+// parameters
 
 function parseParameters(str) {
 	let parameters = str.substr(1).split('&').reduce((p, kv) => {
@@ -25,4 +29,34 @@ function parseParameters(str) {
 		return p
 	}, { })
 	return parameters
+}
+
+// activities
+
+let activities = Array.from(document.querySelectorAll('.activity'))
+let activityLoading = activities.reduce((_, a) => a.classList.contains('loading') ? a : _, null)
+let activityMessage = activities.reduce((_, a) => a.classList.contains('message') ? a : _, null)
+let activityLogin = activities.reduce((_, a) => a.classList.contains('login') ? a : _, null)
+let activityProfile = activities.reduce((_, a) => a.classList.contains('profile') ? a : _, null)
+let activityMain = activities.reduce((_, a) => a.classList.contains('main') ? a : _, null)
+let activityNavBar = activities.reduce((_, a) => a.classList.contains('navbar') ? a : _, null)
+
+// activities switcher
+
+function switchActivity(activity) {
+	activities.forEach(a => a.classList.add('is-hidden'))
+	activity.classList.remove('is-hidden')
+}
+
+// message
+
+function showMessage(title, text, actionCallback) {
+	activityMessage.querySelector('.title').innerText = title
+	activityMessage.querySelector('.text').innerText = text
+	activityMessage.querySelectorAll('.action').forEach(node => {
+		node.onclick = function () {
+			actionCallback();
+		}
+	})
+	switchActivity(activityMessage)
 }
